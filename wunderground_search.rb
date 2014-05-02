85a51b31ce7285cbffcac15a7ee6d874e0682970
Hunter Gillane <hunter.gillane@gmail.com>
2014-05-02 11:22:02 -0600

    move class into new file

---
+++ wunderground_search.rb
@@ -0,0 +1,39 @@
+require "faraday"
+require "json"
+
+class WundergroundSearch
+  def initialize(api_key, base_url)
+    #config
+    @api_key = api_key
+    @base_url = base_url
+  end
+
+  def almanac_search(city, state)
+    # http stuff
+    http_response = Faraday.get(almanac_search_url(city, state))
+
+    # parsing
+    parse_response(http_response.body)
+  end
+
+  private
+
+  def almanac_search_url(city, state)
+    "#{@base_url}/#{@api_key}/almanac/q/#{state}/#{city}.json"
+  end
+
+  def parse_response(response_body)
+    parsed_response = JSON.parse(response_body)
+    average_low_temp = parsed_response["almanac"]["temp_low"]["normal"]["F"]
+    record_low_temp = parsed_response["almanac"]["temp_low"]["record"]["F"]
+    average_high_temp = parsed_response["almanac"]["temp_high"]["normal"]["F"]
+    record_high_temp = parsed_response["almanac"]["temp_high"]["record"]["F"]
+
+    {
+      average_low_temp: average_low_temp,
+      record_low_temp: record_low_temp,
+      average_high_temp: average_high_temp,
+      record_high_temp: record_high_temp
+    }
+  end
+end
