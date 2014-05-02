1727c6b27816b89062364b3bcb1e0360c2cc6875
Hunter Gillane <hunter.gillane@gmail.com>
2014-05-02 11:01:38 -0600

    move printing outside of class

--- almanac.rb
+++ almanac.rb
@@ -18,21 +18,12 @@ class WundergroundSearch
     average_high_temp = parsed_response["almanac"]["temp_high"]["normal"]["F"]
     record_high_temp = parsed_response["almanac"]["temp_high"]["record"]["F"]
 
-    temperature_data = {
+    {
       average_low_temp: average_low_temp,
       record_low_temp: record_low_temp,
       average_high_temp: average_high_temp,
       record_high_temp: record_high_temp
     }
-
-    # printing
-    puts "Results for Denver:"
-
-    puts "Average Low temp for today: #{temperature_data[:average_low_temp]} F"
-    puts "Record Low temp for today: #{temperature_data[:record_low_temp]} F"
-
-    puts "Average High temp for today: #{temperature_data[:average_high_temp]} F"
-    puts "Record High temp for today: #{temperature_data[:record_high_temp]} F"
   end
 
   private
@@ -44,4 +35,13 @@ end
 
 wunderground_client = WundergroundSearch.new("aece37e7e7a48995", "http://api.wunderground.com/api")
 
-wunderground_client.almanac_search("Denver", "CO")
+temperature_data = wunderground_client.almanac_search("Denver", "CO")
+
+# printing
+puts "Results for Denver:"
+
+puts "Average Low temp for today: #{temperature_data[:average_low_temp]} F"
+puts "Record Low temp for today: #{temperature_data[:record_low_temp]} F"
+
+puts "Average High temp for today: #{temperature_data[:average_high_temp]} F"
+puts "Record High temp for today: #{temperature_data[:record_high_temp]} F"
