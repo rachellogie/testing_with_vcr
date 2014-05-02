a0e4d518b13225a892e19fcb8b59c8152fb732fb
Hunter Gillane <hunter.gillane@gmail.com>
2014-05-02 10:55:36 -0600

    move everything into class

--- almanac.rb
+++ almanac.rb
@@ -8,7 +8,24 @@ class WundergroundSearch
   end
 
   def almanac_search(city, state)
-    Faraday.get(almanac_search_url(city, state))
+    # http stuff
+    http_response = Faraday.get(almanac_search_url(city, state))
+
+    # parsing
+    parsed_response = JSON.parse(http_response.body)
+    average_low_temp = parsed_response["almanac"]["temp_low"]["normal"]["F"]
+    record_low_temp = parsed_response["almanac"]["temp_low"]["record"]["F"]
+    average_high_temp = parsed_response["almanac"]["temp_high"]["normal"]["F"]
+    record_high_temp = parsed_response["almanac"]["temp_high"]["record"]["F"]
+
+    # printing
+    puts "Results for Denver:"
+
+    puts "Average Low temp for today: #{average_low_temp} F"
+    puts "Record Low temp for today: #{record_low_temp} F"
+
+    puts "Average High temp for today: #{average_high_temp} F"
+    puts "Record High temp for today: #{record_high_temp} F"
   end
 
   private
@@ -20,22 +37,4 @@ end
 
 wunderground_client = WundergroundSearch.new("aece37e7e7a48995", "http://api.wunderground.com/api")
 
-# HTTP stuff
-http_response = wunderground_client.almanac_search("Denver", "CO")
-
-# parsing
-parsed_response = JSON.parse(http_response.body)
-average_low_temp = parsed_response["almanac"]["temp_low"]["normal"]["F"]
-record_low_temp = parsed_response["almanac"]["temp_low"]["record"]["F"]
-average_high_temp = parsed_response["almanac"]["temp_high"]["normal"]["F"]
-record_high_temp = parsed_response["almanac"]["temp_high"]["record"]["F"]
-
-
-# printing
-puts "Results for Denver:"
-
-puts "Average Low temp for today: #{average_low_temp} F"
-puts "Record Low temp for today: #{record_low_temp} F"
-
-puts "Average High temp for today: #{average_high_temp} F"
-puts "Record High temp for today: #{record_high_temp} F"
+wunderground_client.almanac_search("Denver", "CO")
