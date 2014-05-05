b6891a6a7af811624a5d479a4b1c2a1e3385fbe6
Hunter Gillane <hunter.gillane@gmail.com>
2014-05-05 17:28:26 -0600

    add VCR

--- Gemfile
+++ Gemfile
@@ -2,8 +2,9 @@ source "https://rubygems.org"
 
 gem "faraday", "~> 0.9.0"
 gem "json", "~> 1.8.1"
-gem "webmock", "~> 1.17.4"
 
 group :test do
   gem "rspec", "~> 2.14.1"
+  gem "webmock", "~> 1.17.4"
+  gem "vcr", "~> 2.9.0"
 end
--- spec/wunderground_search_spec.rb
+++ spec/wunderground_search_spec.rb
@@ -3,25 +3,20 @@ require "./wunderground_search"
 
 describe WundergroundSearch do
   describe "#search_almanac" do
-
-    before do
-      stub_request(:get, %r{http://api.wunderground.com/api}).
-        to_return(body: File.read(File.expand_path("./spec/support/almanac_response.json")))
-    end
-
     it "returns some data from wunderground" do
+      VCR.use_cassette("almanac") do
+        wunderground_client = WundergroundSearch.new("aece37e7e7a48995", "http://api.wunderground.com/api")
+        temperature_data = wunderground_client.almanac_search("Denver", "CO")
 
-      wunderground_client = WundergroundSearch.new("aece37e7e7a48995", "http://api.wunderground.com/api")
-      temperature_data = wunderground_client.almanac_search("Denver", "CO")
+        expected_hash = {
+          average_low_temp: 36,
+          record_low_temp: 16,
+          average_high_temp: 62,
+          record_high_temp: 77
+        }
 
-      expected_hash = {
-        average_low_temp: 50,
-        record_low_temp: 42,
-        average_high_temp: 66,
-        record_high_temp: 89
-      }
-
-      expect(temperature_data).to eq(expected_hash)
+        expect(temperature_data).to eq(expected_hash)
+      end
     end
   end
 end
